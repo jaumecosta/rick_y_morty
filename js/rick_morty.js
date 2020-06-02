f1e8700f9ca1ed775ee169da70b692285cbe5a94
jaumecosta <jaumecostagomez@gmail.com>
2020-06-02 19:10:19 +0200

    comentarios añadidos

--- js/rick_morty.js
+++ js/rick_morty.js
@@ -1,20 +1,24 @@
+//URL para acceder a las api
 const API_URL = "https://rickandmortyapi.com/api/";
+
 const PERSONAJES = "character/:id";
 const OPCIONES = { crossDomain: true };
+//el usuario introduce la id para acceder a la api del personaje
 var elegir = prompt("Pon la id del personaje de Rick y Morty que quieres ver");
 
-
+//Esta funcion imprimi el resultado de la api
 const PERSONAJE = function (personaje) {
     document.write(`Hola yo soy ${personaje.name} mi especie es ${personaje.species} y se encuentra en ${personaje.location.name} y soy de ${personaje.origin.name} ${"<br><br>"}`);
 };
 
+//Esta funcion recoje la id y busca en la url las api corresponientes con la id
 function pedirPersonaje(id) {
     const URL = `${API_URL}${PERSONAJES.replace(":id", id)}`;
     $.get(URL, OPCIONES, PERSONAJE);
 }
 
 
-// LAMADAS A LA API_URL
+// Llama a la api url
 pedirPersonaje(elegir);
 
 
